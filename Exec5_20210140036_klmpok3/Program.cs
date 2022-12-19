9b9aba2e89d81f7111d9d318faa1fe9dab1d7f26
Wulan <ASUS@LAPTOP-781R3HVH>
2022-12-19 16:35:50 +0700

    add class node

--- Exec5_20210140036_klmpok3/Program.cs
+++ Exec5_20210140036_klmpok3/Program.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Exec5_20210140036_klmpok3
 {
-    internal class Program
+    internal class node
     {
+        public int rollNumber;
+        public string name;
+        public node next;
+    }
+
         static void Main(string[] args)
         {
         }
